fbabb5f0c223444857c0f5bbeb7b9bbee2f81957
undinag <undina@bu.edu>
2024-07-11 16:57:17 -0400

    update add box warnings

--- nb/get_labels.py
+++ nb/get_labels.py
@@ -2,6 +2,7 @@ import polars as pl
 from glob import glob
 import sys
 import json
+import pandas as pd
 
 # get label information
 
@@ -26,18 +27,18 @@ def available_labels(label_dir):
 def main():
     # get available labels 
     # 2024 file, set_id, spl_version, title
-    label_df = available_labels('./data/2024_latest_labels/')
+    label_df = available_labels('../data/2024_latest_labels/')
 
     # get rxnorm to label file name
     ## rxnorm_mappings: set_id, spl_version, rxcui, rxstring
     ## rxnorm_ingredient: product_rx_cui, product_name, product_omop_concept_id, ingredient_rx_cui, ingredient_name, ingredient_omop_concept_id
-    rxnorm_2024_mappings = pl.read_csv('./data/20240312/rxnorm_mappings.csv'
+    rxnorm_2024_mappings = pl.read_csv('../data/20240312/rxnorm_mappings.csv'
                                        ).select(
                                             pl.col('SETID').alias('set_id'),
                                             pl.col('SPL_VERSION').alias('spl_version'),
                                             pl.col('RXCUI'), #.alias('ingredients_rxcuis'),
                                             pl.col('RXSTRING')) 
-    rxnorm_ingredient = pl.read_csv('./data/20240312/rxnorm_product_to_ingredient.csv')
+    rxnorm_ingredient = pl.read_csv('../data/20240312/rxnorm_product_to_ingredient.csv')
     ## combine into product table
     ## SETID, SPL_VERSION, ingredient_rx_cui, ingredient_name
     product_2024 = (rxnorm_2024_mappings.join(rxnorm_ingredient,
@@ -76,13 +77,25 @@ def main():
                               pl.col('ingredients_names').str.to_lowercase().alias('ingredient_name')
                               )
     )
+
+    box_warnings = (pl.from_pandas(pd.read_csv('/Users/undinagisladottir/Documents/Columbia/Tatonetti_Lab/20231113_onsides/boxed_warnings.csv'))
+                    .filter(pl.col('num_ingredients') == 1)
+                    .select(
+                        pl.col('pt_meddra_id'),
+                        pl.col('pt_meddra_term').str.to_lowercase(),
+                        pl.col('ingredients_rxcuis').cast(pl.Int64).alias('ingredient_rx_cui'),
+                        pl.col('ingredients_names').str.to_lowercase().alias('ingredient_name')
+                        )                
+                    )
+
+    adr_section = pl.concat([adr_section, box_warnings])
     
     onsides_mapped_files = mapped_files.join(adr_section, on=['ingredient_rx_cui', 'ingredient_name'], how='inner')
     print(f"Files in Onsides: {onsides_mapped_files['file'].unique().shape[0]}")
 
     # get mapping between condition_name to meddra_pt terms
     # cohort_id, condition_name, meddra_pt_id, meddra_pt_term, positive_controls (N)
-    meddra_mappings = (pl.read_csv('./data/onsides_mapping.csv')
+    meddra_mappings = (pl.read_csv('../data/onsides_mapping.csv')
                        
     # join with the meddra_pt mappings to get condition_name
                    .select(
@@ -96,7 +109,7 @@ def main():
     onsides_w_ADRs = onsides_mapped_files.join(meddra_mappings, on=['pt_meddra_id', 'pt_meddra_term'], how='inner')
     print(f"Total number of labels w/ ADR in reference set: {onsides_w_ADRs['file'].unique().shape[0]}")    
     ## get reference set
-    pryan_set = (pl.read_csv('./data/pryan_reference_set_ades.csv')
+    pryan_set = (pl.read_csv('../data/pryan_reference_set_ades.csv')
                  .select(
                     pl.col('cohort_id'), 
                     pl.col('condition_name').str.to_lowercase(),
@@ -128,7 +141,7 @@ def main():
                 'reference').unique().sort(['condition_name', 'ingredient_name']).fill_null(0)
         .group_by(['condition_name']).agg(pl.sum('label').alias('labels'), pl.sum('reference').alias('reference'))
         .with_columns(missed = pl.col('reference') - pl.col('labels'))
-    ).write_csv('./data/label_reference_comparison.csv')
+    ).write_csv('../data/label_reference_comparison.csv')
 
     # create table with all ADR and drug pairs and whether they were captured or not
     (pryan_set.with_columns(reference = 1).join(
@@ -139,14 +152,14 @@ def main():
                 'ingredient_name',
                 'label',
                 'reference').unique().sort(['condition_name', 'ingredient_name']).fill_null(0)
-    ).write_csv('./data/label_reference_drugs_captured.csv')
+    ).write_csv('../data/label_reference_drugs_captured.csv')
 
     # write table breakdown of positive, negative, and non-positive drugs
     (pryan_onsides.group_by('condition_name', 'affect')
           .agg(pl.col('ingredient_name').n_unique().alias('unique_drugs'))
-          .sort('condition_name', 'affect')).write_csv('./data/ADR_drug_breakdown.csv')
+          .sort('condition_name', 'affect')).write_csv('../data/ADR_drug_breakdown.csv')
     
     # write table with all label names and ADRs and pos/non-pos
-    pryan_onsides.unique().write_csv('./data/labels_w_ADRs.csv')
+    pryan_onsides.unique().write_csv('../data/labels_w_ADRs.csv')
     
 main()
